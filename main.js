3f70c2b77da63b74a924c703a95f004bcb5d4514
carmentudela <ctudelamedel@gmail.com>
2022-02-17 10:58:26 +0100

    antes de cambiar la forma de añadir los elementos

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-var botonSiguiente = document.getElementById('siguiente');
-botonSiguiente.addEventListener('click', showJoke);
-
+var botonSiguiente = document.getElementById('siguiente'); //Creates a const with the button id.
+botonSiguiente.addEventListener('click', showJoke); //Asings the function showJoke to the click event.
+var pantalla = document.getElementById('joke');
+var plantilla = document.createElement('div');
+var reportAcudits = [];
+//Function to print the joke on the console, when clicking the 'següent acudit' button.
 function showJoke() {
     var UrlApi = 'https://icanhazdadjoke.com';
-    fetch("".concat(UrlApi), {
-            headers: {
-                'accept': 'application/json'
-            }
-        })
-        .then(function (resp) {
-            return resp.json();
-        })
+    fetch("".concat(UrlApi), { headers: { 'accept': 'application/json' } })
+        .then(function (resp) { return resp.json(); })
         .then(function (data) {
-
-            console.log("\"".concat(data.joke, "\""));
-        });
+        var element = document.createElement('span');
+        element.classList.add('span');
+        element.appendChild(document.createTextNode("".concat(data.joke)));
+        var divBotones = document.createElement('div');
+        divBotones.classList.add('divBotones');
+        var emoji1 = document.createElement('button');
+        emoji1.innerHTML = '&#128546';
+        emoji1.classList.add('emoji1');
+        var id1 = document.querySelector('.emoji1');
+        if (id1) {
+            id1.addEventListener('click', nolo);
+            function nolo() {
+                console.log('hey');
+            }
+        }
+        var boton2 = document.createElement('button');
+        boton2.innerHTML = 'Mmm';
+        var boton3 = document.createElement('button');
+        boton3.innerHTML = 'Good';
+        divBotones.appendChild(emoji1);
+        divBotones.appendChild(boton2);
+        divBotones.appendChild(boton3);
+        element.appendChild(divBotones);
+        plantilla.appendChild(element);
+    });
+    pantalla.appendChild(plantilla);
+    removeJoke();
+}
+function removeJoke() {
+    plantilla.innerHTML = '';
 }
--- main.ts
+++ main.ts
@@ -2,16 +2,53 @@
 
 const botonSiguiente = document.getElementById('siguiente');//Creates a const with the button id.
 botonSiguiente.addEventListener('click', showJoke);//Asings the function showJoke to the click event.
+const pantalla = document.getElementById('joke');
+const plantilla = document.createElement('div');
+
+const reportAcudits: Array<{ joke: string, score: number, date: Date }> = [];
 
 //Function to print the joke on the console, when clicking the 'següent acudit' button.
 
 function showJoke() {
+
     const UrlApi: string = 'https://icanhazdadjoke.com';
     fetch(`${UrlApi}`, { headers: { 'accept': 'application/json' } })
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(`"${data.joke}"`);
-        });
 
+            let element = document.createElement('span');
+            element.classList.add('span');
+            element.appendChild(document.createTextNode(`${data.joke}`));
+            let divBotones = document.createElement('div');
+            divBotones.classList.add('divBotones');
+            let emoji1 = document.createElement('button');
+            emoji1.innerHTML = '&#128546';
+            emoji1.classList.add('emoji1');
+            const id1 = document.querySelector('.emoji1');
+            if (id1) {
+
+
+                id1.addEventListener('click', nolo);
+                function nolo(): void {
+                    console.log('hey');
+                }
+            }
+
+            let boton2 = document.createElement('button');
+            boton2.innerHTML = 'Mmm';
+            let boton3 = document.createElement('button');
+            boton3.innerHTML = 'Good';
+            divBotones.appendChild(emoji1);
+            divBotones.appendChild(boton2);
+            divBotones.appendChild(boton3);
+            element.appendChild(divBotones);
+            plantilla.appendChild(element);
+        });
+    pantalla.appendChild(plantilla);
+    removeJoke();
 
 }
+function removeJoke() {
+    plantilla.innerHTML = '';
+}
+
